3dfb7d6888e04024d3943d7cb9a9cfca08ca3fa4
SSR14FEB <instaheri6@gmail.com>
2024-09-24 19:44:44 +0530

    basic work done

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require('dotenv').config()
+const { configDotenv } = require('dotenv')
 const express = require('express')
 const app = express()
-const port = 4000
+const port = 3000
+
+const sports={"sports":[{"id":1,"value":"Soccer"},{"id":2,"value":"Basketball"},{"id":3,"value":"Tennis"},{"id":4,"value":"Swimming"},{"id":5,"value":"Cricket"},{"id":6,"value":"Table Tennis"},{"id":7,"value":"Golf"},{"id":8,"value":"Rugby"},{"id":9,"value":"Badminton"},{"id":10,"value":"Baseball"},{"id":11,"value":"Fencing"},{"id":12,"value":"Volleyball"},{"id":13,"value":"Martial Arts"},{"id":14,"value":"Cycling"}]}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -10,6 +14,10 @@ app.get('/backend',(req,res)=>{
     res.send("Hello backend")
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/sports',(req,res)=>{
+    res.json(sports)
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
 })
