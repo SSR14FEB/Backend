6b9ec1bcd22fe344223a3841b7b77af0b7d04bef
SSR14FEB <instaheri6@gmail.com>
2024-11-14 10:09:48 +0530

    add user and video models

---
+++ src/models/user_models.js
@@ -0,0 +1,97 @@
+import mongoose, { Schema } from "mongoose";
+
+import jwt from "jsonwebtoken"
+
+import bcrytjs from "bcryptjs"
+
+const userShema = new Schema(
+  {
+    userName: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      required: [true, "user id is required"],
+      index: ture,
+    },
+    email: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      required: [true, "email is required"],
+    },
+    fullName: {
+      type: String,
+      trim: true,
+      required: [true, "user id is required"],
+      index: true,
+    },
+    avtar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "video",
+      },
+    ],
+    password: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      required: [true, "password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+// passowrd dicrption use of pre hook it is a mongo middleware
+userShema.pre("save", function async (next){
+
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt(this.password,10)
+    next()
+
+})
+
+// creating methoud to compare the password
+userShema.methods.isPasswordCorrect = async function(passowrd){
+    return bcrypt.compare(passowrd,this.passowrd)
+}
+
+// gentration of access tokens and refresh tokens
+userShema.methods.generateAccessToken = function(){
+  return jwt.sign(
+    {
+      _id:this.id,
+      email:this.email,
+      userName:this.userName,
+      fullName:this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn:process.env.ACCESS_TOKEN_EXPIREY
+    }
+  )
+}
+
+userShema.methods.generateRefreshToken = function(){
+  return jwt.sign(
+    {
+      _id:this.id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
+
+export const User = mongoose.model("User", userShema);
